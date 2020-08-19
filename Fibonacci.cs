205fe65ce287d1352ebc5563a36390b73e8fd3af
马文泽 <wenze.ma@yingxiong.com>
2020-08-19 22:07:28 +0800

    斐波那契数列

---
+++ Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿public class Fibonacci
+{
+	public int Fib(int N) 
+	{
+        if(N < 1) return 0;
+        if(N <= 2) return 1;
+        int prev = 1;
+        int cur = 1;
+        int sum = 0;
+        for(int i = 3;i<=N;i++)
+        {
+            sum = prev + cur;
+            prev = cur;
+            cur = sum;
+        }
+        return cur;
+    }
+}
